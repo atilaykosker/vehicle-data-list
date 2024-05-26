def7316184a317654c647cb2ca1b0982c831ec6d
Atilay <atovarkosker@gmail.com>
2024-05-26 15:45:55 +0300

    feat: add suspense and loading page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css';
 
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import { Suspense } from 'react';
 
 import ReactQueryProvider from '@/providers/ReactQueryProvider';
 
@@ -21,7 +22,9 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ReactQueryProvider>
-          <main className="flex min-h-screen flex-col items-center justify-between p-4">{children}</main>
+          <main className="flex min-h-screen flex-col items-center justify-between p-4">
+            <Suspense>{children}</Suspense>
+          </main>
           <div id="modal-root"></div>
         </ReactQueryProvider>
       </body>
